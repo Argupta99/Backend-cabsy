44e1c0a2711af00671e92c87d5772bc0f707aa74
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2025-06-03 18:42:58 +0530

    installed dotenv and cors

---
+++ app.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
+//const userRoutes = require('./routes/user.route');
+
+
+connectToDb();
+
+
+//config cors
+app.use(cors());
+app.use(express.json());
+
+//urlencoded
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+//config userRoutes
+//app.use('/users', userRoutes);
+
+module.exports = app;
